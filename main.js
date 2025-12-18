6aca23accdae212faf1f4b881eaca90f642f033c
Jan Pokorný <JenomPokorny@gmail.com>
2025-12-18 22:07:34 +0100

    chore: set up project
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

---
+++ main.js
@@ -0,0 +1,10 @@
+const { app, BrowserWindow } = require("electron")
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    show: false,
+    fullscreen: true,
+  })
+  win.loadFile('index.html')
+  win.show()
+})
